7ab4b2ad9e4e9d967bf6ae1d14c83cbf52d274a2
suxuelengyin <suxuewb@gmail.com>
2018-11-01 17:44:59 +0800

    11.1

--- fronend/imatch/src/pages/game/index.js
+++ fronend/imatch/src/pages/game/index.js
@@ -5,68 +5,69 @@ import { Table } from 'antd';
 import { Link } from 'react-router-dom';
 import { lookup } from '@/utils/tools'
 const columns = [
-  {
-    title: '赛事名称',
-    dataIndex: 'name',
-    width: 150,
-    render:(text,record)=>{
-      return (
-        <Link
-        to={{
-          pathname:'/details/dhome',
-          search: `?id=${record.id}`
-        }}
-         target="_black"
-        >
-          {text}
-        </Link>
-      )
-    }
-  },
-  {
-    title: '举办方',
-    dataIndex: 'age',
-    width: 150,
-  },
-  {
-    title: '时间',
-    dataIndex: 'time',
-    width: 150,
-  }, {
-    title: '类型',
-    dataIndex: 'tags',
-    width: 150,
-  }, {
-    title: '备注',
-    dataIndex: 'ps',
-    width: 150,
-  },]
+	{
+		title: '赛事名称',
+		dataIndex: 'name',
+		width: 150,
+		render: (text, record) => {
+			return (
+				<Link
+					to={{
+						pathname: '/details/dhome',
+						search: `?id=${record.id}`
+					}}
+					target="_black"
+				>
+					{text}
+				</Link>
+			)
+		}
+	},
+	{
+		title: '举办方',
+		dataIndex: 'age',
+		width: 150,
+	},
+	{
+		title: '时间',
+		dataIndex: 'time',
+		width: 150,
+	}, {
+		title: '类型',
+		dataIndex: 'tags',
+		width: 150,
+	}, {
+		title: '备注',
+		dataIndex: 'ps',
+		width: 150,
+	},]
 class TeamList extends Component {
-  componentWillMount() {
-    const { dispatch } = this.props;
-    dispatch({
-      type: 'ogGame/search',
-      payload: {}
-    })
-  }
-  getdata = (model) => {//获取数据
-    const { ids } = this.props[model];
-    const data = this.props.odooData[model];
-    const dataSource = lookup(ids, data);
-    return dataSource
-  }
-  render() {
-    const dataSource = this.getdata('ogGame')
-    return (
-      <div className={styles.normal}>
-        <h1><Table
-        rowKey={row=>row.id}
-          columns={columns}
-          dataSource={dataSource}
-          pagination={{ pageSize: 5 }}
-          scroll={{ y: 300 }} /></h1>
-      </div>)
-  }
+	componentWillMount() {
+		const { dispatch } = this.props;
+		dispatch({
+			type: 'ogGame/search',
+			payload: {}
+		})
+	}
+	getdata = (model) => {//获取数据
+		const { ids } = this.props[model];
+		const data = this.props.odooData[model];
+		const dataSource = lookup(ids, data);
+		return dataSource
+	}
+	render() {
+		const dataSource = this.getdata('ogGame')
+
+		return (
+			<div className={styles.normal}>
+				<h1><Table
+					rowKey={row => row.id}
+					columns={columns}
+					dataSource={dataSource}
+					pagination={{ pageSize: 5 }}
+					scroll={{ y: 300 }} /></h1>
+			</div>)
+	}
 }
 
 
