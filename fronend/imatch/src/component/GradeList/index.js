aef92402a9e5e146d7d4997fec60aec77c59946d
Karmaliu <2134515368@qq.com>
2018-11-02 10:00:20 +0800

    页面修改

--- fronend/imatch/src/component/GradeList/index.js
+++ fronend/imatch/src/component/GradeList/index.js
@@ -3,8 +3,17 @@
 import React from 'react';
 import { Table } from 'antd';
 import { Link } from 'dva/router';
-
-const GradeList = ({ gradeData }) => {
+const GradeList = ({ dataSource }) => {
+    console.log(dataSource);
+    
+    const dataSource1=[...dataSource];
+    dataSource1.map((item)=>{
+        const dateBegin=item.date_from.split(" ");
+        const dataEnd=item.date_thru.split(" ")
+        item.date=dateBegin[0];
+        item.time=dateBegin[1]+'~'+dataEnd[1]
+        return item
+    })
     const columns = [{
         title: "日期",
         dataIndex: "date",
@@ -31,19 +40,22 @@ const GradeList = ({ gradeData }) => {
        
     }, {
         title: "公开团体赛",
-        dataIndex: 'gameteam',
+        dataIndex: 'name',
         align:"center",
         render: (text, row) => {
             return (
-                <Link to="/details/grade/graresult">{text}</Link>
+                <Link 
+                to={{pathname:'/details/grade/graresult',search:`id=${row.id}`}}
+                >{text}</Link>
             )
         }
     }]
     return (
         <div style={{ width:'700px',margin:"0 auto"}}>
             <Table
+            rowKey={row=>row.id}
             columns={columns}
-            dataSource={gradeData}
+            dataSource={dataSource1}
             bordered={true}
             />
         </div>
--- fronend/imatch/src/pages/details/grade/index.js
+++ fronend/imatch/src/pages/details/grade/index.js
@@ -1,17 +1,42 @@
 //  成绩表
 //  LSY
 //  2018-9-6
+import React,{Component} from 'react'
+import {connect} from 'dva'
+import {lookup} from '@/utils/tools'
 import styles from './index.css';
 import GradeList from '../../../component/GradeList';
 import * as detail from '../../../../mock/detail';
+import Search from 'antd/lib/input/Search';
 
 const { gradeData } = detail;
-export default function (props) {
-    return (
-        <div className={styles.normal}>
+class grade extends Component{
+    componentWillMount(){
+        const dispatch=this.props.dispatch;
+        const {query}=this.props.location
+        const id=query.id
+        dispatch({
+            type:"ogGameRound/search",
+            payload:{}
+        })
+    }
+    getdata = (model) => {//获取数据
+		const { ids } = this.props[model];
+		const data = this.props.odooData[model];
+		const dataSource = lookup(ids, data);
+		return dataSource
+	}
+    render(){
+        const dataSource=this.getdata('ogGameRound')
+        console.log(dataSource);
+        
+        return(
+            <div className={styles.normal}>
             <GradeList
-                gradeData={gradeData}
+                dataSource={dataSource}
             />
         </div>
-    );
+        )
+    }
 }
+export default connect(({odooData,ogGameRound})=>({odooData,ogGameRound}))(grade)
